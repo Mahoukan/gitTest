477477679ef53b75ac2649fa976026cf1be9794f
Mahoukan <suriyakrishnans@gmail.com>
2025-07-05 01:47:38 +1200

    Updated website styling and buttons

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function sayHi() {
-  alert("Hi from JavaScript!");
+function startGame() {
+  document.getElementById('output').innerText = "Starting game...";
+  // Replace this with actual game logic
+}
+
+function showInstructions() {
+  document.getElementById('output').innerText = "Use arrow keys to move. Avoid obstacles!";
+}
+
+function showCredits() {
+  document.getElementById('output').innerText = "Created by Your Name.";
 }
